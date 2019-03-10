c422ce5176a6b6a543cdcdc478a0a3093ecf2b35
“markieruiz” <mjsifton@gmail.com>
2019-03-10 08:56:16 -0500

    Fixing num of wins

--- clickygame/src/App.js
+++ clickygame/src/App.js
@@ -40,7 +40,7 @@ class App extends Component {
   }
 
   turn = (id) => {
-    if (this.state.score === 4) {
+    if (this.state.score === 11) {
       this.setState({
         winText: "Congratulations!",
         headerText: "",
