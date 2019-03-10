3aae178c6defb40bdd77c109bee831ea7a480587
“markieruiz” <mjsifton@gmail.com>
2019-03-10 10:31:05 -0500

    getting published ready

--- src/App.js
+++ src/App.js
@@ -40,7 +40,7 @@ class App extends Component {
   }
 
   turn = (id) => {
-    if (this.state.score === 11) {
+    if (this.state.score === 2) {
       this.setState({
         winText: "Congratulations!",
         headerText: "",
