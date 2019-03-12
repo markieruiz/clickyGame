a96cc8b8c6f908b6193e7b80e987ccdb6667b7ec
“markieruiz” <mjsifton@gmail.com>
2019-03-12 15:07:36 -0500

    Updates

---
+++ precache-manifest.0e8205bf10711f817d0143e753da6e87.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3fe81c4c31232682d87b",
+    "url": "/clickyGame/static/js/runtime~main.62255258.js"
+  },
+  {
+    "revision": "0c2fcf2632baec5650cc",
+    "url": "/clickyGame/static/js/main.ddaedb18.chunk.js"
+  },
+  {
+    "revision": "4f45a4c5141b0ffce190",
+    "url": "/clickyGame/static/js/2.f6dbb215.chunk.js"
+  },
+  {
+    "revision": "0c2fcf2632baec5650cc",
+    "url": "/clickyGame/static/css/main.6f4e7489.chunk.css"
+  },
+  {
+    "revision": "dcc23d413baefa81a27061820580081c",
+    "url": "/clickyGame/index.html"
+  }
+];
